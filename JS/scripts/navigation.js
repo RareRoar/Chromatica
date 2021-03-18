d6581991a8d5d66a3ccda181316ced414d84d514
RareRoar <sirdysprozium@gmail.com>
2021-03-18 13:53:33 +0300

    Added JS folder

---
+++ JS/scripts/navigation.js
@@ -0,0 +1,13 @@
+async function loadSlide(slideName) {
+    const response = await fetch(`../JS/slides/${slideName}.html`);
+    let slideBlock = document.createElement('div');
+    slideBlock.innerHTML = await response.text();
+    console.log(slideBlock.innerHTML)
+    document.body.insertBefore(slideBlock, document.getElementsByTagName('footer')[0]);
+}
+
+//loadSlide(links[i].dataset.slideName)
+let links = document.getElementsByClassName('nav-item-left');
+for(let i=0; i<links.length; i++){
+    links[i].onclick = loadSlide('inspiration');
+}
