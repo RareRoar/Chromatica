d710ba931dbd16e202854dbcd8ab9b60b9872327
RareRoar <sirdysprozium@gmail.com>
2021-03-20 21:06:11 +0300

    Navigation test 0

--- JS/scripts/navigation.js
+++ JS/scripts/navigation.js
@@ -1,3 +1,4 @@
+/*
 async function loadSlide(slideName) {
     const response = await fetch(`../JS/slides/${slideName}.html`);
     
@@ -8,7 +9,6 @@ async function loadSlide(slideName) {
     document.body.insertBefore(slideBlock, document.getElementsByTagName('footer')[0]);
 }
 
-//loadSlide(links[i].dataset.slideName)
 let links = document.getElementsByClassName('nav-item-left');
 for(let i=0; i<links.length; i++){
     links[i].onclick = function() { 
@@ -17,5 +17,120 @@ for(let i=0; i<links.length; i++){
             oldSlideBlock.parentNode.removeChild(oldSlideBlock);
         }
         loadSlide(links[i].dataset.slideName);
+    } ;
+}
+*/
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                return rawFile.responseText;
+                
+            }
+        }
+    }
+}
+
+class SlideManager {
+
+    constructor() {
+        this.slideTitleList = ['presentation', 'inspiration', 'collection', 'spectrum', 'authentication'];
+        this.loadSlides();
+        this.slides = [];
+    }
+
+
+    async loadSlide(slideTitle) {
+        const response = await fetch(`./slides/${slideTitle}.html`);
+        const slideContent = await response.text();
+        return new Slide(slideTitle, slideContent);
+    }
+
+    async loadSlides() {
+        for (let slideTitle of this.slideTitleList) {
+
+            const nextSlide = await this.loadSlide(slideTitle);
+            this.slides.push(nextSlide);
+        }
+    }
+}
+
+class Slide {
+
+    constructor(title, htmlContent) {
+        this.title = title;
+        this.htmlContent = htmlContent;
+    }
+
+    get html() {
+        return this.htmlContent;
+    }
+
+    set html(value) {
+        this.htmlContent = value;
+    }
+}
+
+class Navigator {
+
+    constructor() {
+        this.initialize();
+        this.configureOnPopHandler();
+        this.state = {
+            currentSlide: null,
+        };
+    }
+
+    initialize() {
+        window.history.replaceState(this.state, null, "");
+    }
+
+    configureOnPopHandler() {
+        window.onpopstate = function (event) {
+            if (event.state) { state = event.state; }
+            this.render();
+        };
+    }
+
+    pushSlide(slide) {
+        this.state.currentSlide = slide;
+        window.history.pushState(this.state, null, "");
+        this.render();
+    }
+
+
+    render() {
+        let oldSlideBlock = document.getElementById('slide-block');
+        if (oldSlideBlock != null) {
+            oldSlideBlock.parentNode.removeChild(oldSlideBlock);
+        }
+        let slideBlock = document.createElement('div');
+        slideBlock.id = 'slide-block';
+        if (currentSlide != undefined) {
+            slideBlock.innerHTML = currentSlide.html;
+            document.body.insertBefore(slideBlock, document.getElementsByTagName('footer')[0]);
+        }
+        else {
+            console.log('ERROR: Current slide is undefined!');
+        }
+    }
+}
+
+const nav = new Navigator();
+const sl = new SlideManager();
+console.log(sl.slides);
+let links = document.getElementsByClassName('nav-item-left');
+for(let i=0; i<links.length; i++){
+    links[i].onclick = function() {
+        
+        nav.pushSlide(sl.slides[i]);
+
     } ;
 }
